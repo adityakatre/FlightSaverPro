386469379f9b00c2014b4eaf1260db2242b9e73d
aditya katre <adityakatre007@gmail.com>
2024-02-15 13:48:24 +0530

    modification made in scraper.py for all 3 site scarping code runing concurrently

--- scraper.py
+++ scraper.py
@@ -1,9 +1,15 @@
 # scraper.py
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 import re
+
+chrome_options = Options()
+chrome_options.add_argument("--headless") 
 
 def extract_flight_data_EaseMyTrip(elementsoup):
     # Extracting flight price
@@ -103,59 +109,77 @@ def extract_price_value(flight_data):
 
 
 
-def scrape_data():
-    # EaseMyTrip
-    driver_emt = webdriver.Chrome()
-    url_emt = "https://flight.easemytrip.com/FlightList/Index?srch=PNQ-Pune-India|DEL-Delhi-India|01/03/2024&px=1-0-0&cbn=0&ar=undefined&isow=true&isdm=true&lang=en-us&&IsDoubleSeat=false&CCODE=IN&curr=INR&apptype=B2C"
-    driver_emt.get(url_emt)
+def scrape_emt():
+    driver = webdriver.Chrome()
+    url = "https://flight.easemytrip.com/FlightList/Index?srch=PNQ-Pune-India|DEL-Delhi-India|01/03/2024&px=1-0-0&cbn=0&ar=undefined&isow=true&isdm=true&lang=en-us&&IsDoubleSeat=false&CCODE=IN&curr=INR&apptype=B2C"
+    driver.get(url)
     time.sleep(5)
-    flight_rows_emt = driver_emt.find_elements(By.XPATH, "//div[contains(@class,'col-md-12 col-sm-12 main-bo-lis pad-top-bot ng-scope')]")
-    data_list_emt = []
+    flight_rows = driver.find_elements(By.XPATH, "//div[contains(@class,'col-md-12 col-sm-12 main-bo-lis pad-top-bot ng-scope')]")
+    data_list = []
 
-    for WebElement in flight_rows_emt:
+    for WebElement in flight_rows:
         elementHTML = WebElement.get_attribute("outerHTML")
         elementsoup = BeautifulSoup(elementHTML, 'html.parser')
-        flight_data_emt = extract_flight_data_EaseMyTrip(elementsoup)
-        data_list_emt.append(flight_data_emt)
+        flight_data = extract_flight_data_EaseMyTrip(elementsoup)
+        data_list.append(flight_data)
 
-    driver_emt.quit()
+    driver.quit()
+    return data_list
 
-    # Agoda
-    driver_agoda = webdriver.Chrome()
-    url_agoda = "https://www.agoda.com/en-in/flights/results?cid=1844104&departureFrom=PNQ&departureFromType=1&arrivalTo=DEL&arrivalToType=1&departDate=2024-03-01&returnDate=2024-03-02&searchType=1&cabinType=Economy&adults=1&sort=8"
-    driver_agoda.get(url_agoda)
+def scrape_agoda():
+    driver = webdriver.Chrome()
+    url = "https://www.agoda.com/en-in/flights/results?cid=1844104&departureFrom=PNQ&departureFromType=1&arrivalTo=DEL&arrivalToType=1&departDate=2024-03-01&returnDate=2024-03-02&searchType=1&cabinType=Economy&adults=1&sort=8"
+    driver.get(url)
     time.sleep(5)
-    flight_cards_agoda = driver_agoda.find_elements(By.XPATH, "//div[contains(@class, 'Box-sc-kv6pi1-0 bhCeID')]")
-    data_list_agoda = []
+    flight_cards = driver.find_elements(By.XPATH, "//div[contains(@class, 'Box-sc-kv6pi1-0 bhCeID')]")
+    data_list = []
 
-    for card in flight_cards_agoda:
+    for card in flight_cards:
         card_html = card.get_attribute("outerHTML")
         card_soup = BeautifulSoup(card_html, 'html.parser')
-        flight_data_agoda = extract_flight_data_agoda(card_soup)
-        data_list_agoda.append(flight_data_agoda)
+        flight_data = extract_flight_data_agoda(card_soup)
+        data_list.append(flight_data)
 
-    driver_agoda.quit()
-    
-    # Yatra
-    driver_yatra = webdriver.Chrome()
-    url_yatra = "https://flight.yatra.com/air-search-ui/dom2/trigger?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=PNQ&originCountry=IN&destination=DEL&destinationCountry=IN&flight_depart_date=01%2F03%2F2024&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-flights&unqvaldesktop=454230595489"
-    driver_yatra.get(url_yatra)
+    driver.quit()
+    return data_list
+
+def scrape_yatra():
+    driver = webdriver.Chrome()
+    url = "https://flight.yatra.com/air-search-ui/dom2/trigger?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=PNQ&originCountry=IN&destination=DEL&destinationCountry=IN&flight_depart_date=01%2F03%2F2024&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-flights&unqvaldesktop=454230595489"
+    driver.get(url)
     time.sleep(5)
-    flight_cards_yatra = driver_yatra.find_elements(By.XPATH, "//div[contains(@class ,'flightItem border-shadow pr')]")
-    data_list_yatra = []
+    flight_cards = driver.find_elements(By.XPATH, "//div[contains(@class ,'flightItem border-shadow pr')]")
+    data_list = []
 
-    for card in flight_cards_yatra:
+    for card in flight_cards:
         card_html = card.get_attribute("outerHTML")
         card_soup = BeautifulSoup(card_html, 'html.parser')
-        flight_data_yatra = extract_flight_data_yatra(card_soup)
-        data_list_yatra.append(flight_data_yatra)
+        flight_data = extract_flight_data_yatra(card_soup)
+        data_list.append(flight_data)
 
-    driver_yatra.quit()
+    driver.quit()
+    return data_list
 
-    # Combine data
+def scrape_data():
+    # Running scraping functions in parallel
+    with ThreadPoolExecutor() as executor:
+        emt_future = executor.submit(scrape_emt)
+        agoda_future = executor.submit(scrape_agoda)
+        yatra_future = executor.submit(scrape_yatra)
+
+    # Combine data from all sites
+    data_list_emt = emt_future.result()
+    data_list_agoda = agoda_future.result()
+    data_list_yatra = yatra_future.result()
     combined_data = data_list_emt + data_list_agoda + data_list_yatra
-    
+
     # Sort combined data by flight price
     combined_data_sorted = sorted(combined_data, key=extract_price_value)
-    
+
     return combined_data_sorted
+
+
+
+
+
+
